3a732cf237cce078a4c885b1cd6a98fe435fec94
harini-si <hariniskumar10@gmail.com>
2023-06-09 13:59:04 +0000

    user verbalization

--- videoCopilot/verbalise.py
+++ videoCopilot/verbalise.py
@@ -1,27 +1,45 @@
 from typing import List, Any, Tuple
 import random
+import pandas as pd
 
 class AdMemUserVerbalisation:
-    def __init__(self, user_id, study_id) -> None:
-        pass
-    
+    def __init__(self, user_id, study_id,path) -> None:
+        self.user_id=user_id
+        self.path= path
+        self.study_id=study_id
+        self.df= pd.read_csv(self.path+f"users_study{study_id}.csv")
+        self.row= self.df.loc[self.df['user_id'] == self.user_id]
+        
     def verb_persona(self) -> str:
         '''
         pass locale, age, and gender.
         '''
-        return ""
+        age= '20'
+        gender= self.row['Gender'].values[0]
+        locale='India'
+        return f"{age} year old {gender} from {locale}"
     
     def verb_seen_advertisements(self, watched: bool, n: int) -> str:
         '''
         watched = True/False means the viewer selected/unselected the brand, n is the max number of brands to return.
         '''
-        return ""
+        if watched:
+            out= self.row['brands_seen'].values[0]
+        else:
+            out= set(self.row['brands_seen_options'].values[0])-set(self.row['brands_seen'].values[0])
+            out= random.sample(out, n)
+        return f"{out}"
     
     def verb_used_products(self, used: bool, n: int) -> str:
         '''
         watched = True/False means the viewer selected/unselected the brand, n is the max number of brands to return.
         '''
-        return ""
+        if used:
+            out= self.row['brands_used'].values[0]
+        else:
+            out= set(self.row['brands_used_options'].values[0])-set(self.row['brands_used'].values[0])
+            out= random.sample(out, n)
+        return f"{out}"
     
     def verb_ad_blocker_yt_sub(self) -> str:
         '''
@@ -30,8 +48,9 @@ class AdMemUserVerbalisation:
         The viewer doesn't use an ad blocker but is a YouTube Premium subscriber OR
         The viewer doesn't use an ad blocker and isn't a YouTube Premium subscriber
         '''
-        
-        return ""
+        adblocker= 'uses an' if self.row['ad_block'].values[0] else 'doesn\'t use an'
+        youtube_sub= 'is' if self.row['yt_sub'].values[0] else 'isn\'t'
+        return f"The viewer {adblocker} ad blocker and {youtube_sub} a YouTube Premium subscriber"
     
     def verb_time_on_yt_mobile_web(self) -> str:
         '''
@@ -40,13 +59,15 @@ class AdMemUserVerbalisation:
         The viewer spends more time on web than mobile on YouTube OR
         The viewer spends more time on mobile than web on YouTube
         '''
-        return ""
+        youtube_mobile= self.row['yt_mobile'].values[0] 
+        return f"The viewer spends {youtube_mobile} on YouTube"
     
     def verb_primary_info_source(self, apprise:bool, n:int) -> str:
         '''
         apprise = True/False means the viewer selected/unselected the method, n is the max number of methods to return.
         '''
-        return ""
+        apprise= self.row['apprise'].values[0]
+        return f"{apprise}"
         
     def verbalise(self) -> List[Tuple[str, float]]:
         return [
@@ -55,7 +76,7 @@ class AdMemUserVerbalisation:
             (f"They remember using products like {self.verb_used_products(used=True, n=5)} but not {self.verb_used_products(used=False, n=5)}.", 1),
             (f"The viewer {self.verb_ad_blocker_yt_sub()}.", 1),
             (f"They spend {self.verb_time_on_yt_mobile_web()}.", 1),
-            (f"They primarily apprise themselves of the latest products and brands through {self.verb_primary_info_source(apprise=True, n=3)}, but not via {self.verb_primary_info_source(apprise=False, n=3)}.", 1),
+            (f"They primarily apprise themselves of the latest products and brands through {self.verb_primary_info_source()}", 1),
         ]
     
     def __call__(self) -> str:
@@ -63,10 +84,19 @@ class AdMemUserVerbalisation:
         return " ".join([entry[0] for entry in entries if random.random() < entry[1]])
         
     
+    
 class AdMemVideoVerbalisation:
     def __init__(self, video_id) -> None:
         pass
     
+    def verbalise(self) -> List[Tuple[str, float]]:
+        return [
+            (f"They watch a {self.video_length} second advertisement for {self.brand}.", 1),
+            (f"The video is shot in {self.get_orientation()}.", 1),
+            
+            
+        ]
+
     def __call__(self) -> List[str]:
         return ""
     
